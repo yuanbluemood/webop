839721fa73753929ff9598399d4d745501f1a1f1
yuanbluemood <liuyuan6666@gmail.com>
2014-12-10 11:42:38 +0800

    Update install_ffmpeg_rhel.sh

--- maintain/install_ffmpeg_rhel.sh
+++ maintain/install_ffmpeg_rhel.sh
@@ -2,7 +2,7 @@
 cat >/etc/yum.repos.d/dag.repo <<EOF
 [dag]
 name=Dag RPM Repository for Red Hat Enterprise Linux
-baseurl=http://apt.sw.be/redhat/el$releasever/en/\$basearch/dag
+baseurl=http://apt.sw.be/redhat/el\$releasever/en/\$basearch/dag
 gpgcheck=1
 enabled=1
 EOF
