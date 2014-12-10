f648192e79dffacc0a1fc9538edfaad78fad1ef3
yuanbluemood <liuyuan6666@gmail.com>
2014-12-10 11:47:24 +0800

    Update install_ffmpeg_rhel.sh

--- maintain/install_ffmpeg_rhel.sh
+++ maintain/install_ffmpeg_rhel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/yuanbluemood/webop/master/maintain/dag.repo -o >/etc/yum.repos.d/dag.repo
+curl https://raw.githubusercontent.com/yuanbluemood/webop/master/maintain/dag.repo -o /etc/yum.repos.d/dag.repo
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt;
 yum -y install ffmpeg ffmpeg-devel;
